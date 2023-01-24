673cd81b7b4adc663ef1e9c9cbd7ec3142045b61
Master <Master@LAPTOP-4EHGBCL4>
2023-01-24 12:59:53 +0000

    config

---
+++ ThAmCo-CommerceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ThAmCo-CommerceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThAmCo-CommerceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("753c7b52-9ba0-4182-b5c7-66c17d500938")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThAmCo-CommerceTests1/Controllers/ProductControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThAmCo_Commerce.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThAmCo_Commerce.Controllers.Tests
+{
+    [TestClass()]
+    public class ProductControllerTests
+    {
+        [TestMethod()]
+        public void ProductControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConformationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
